b91b286fb4cf8f0e980e37c7143d65c5184d12d7
JumaOchi <ochiengjuma95@gmail.com>
2025-05-03 04:13:16 +0300

    maze solve

--- tests.py
+++ tests.py
@@ -52,6 +52,16 @@ class Tests(unittest.TestCase):
         self.assertFalse(m._cells[m._num_cols - 1][m._num_rows - 1].has_bottom_wall)
 
     
+    def test_maze_reset_cells_visited(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        for col in m1._cells:
+            for cell in col:
+                self.assertEqual(
+                    cell._visited,
+                    False,
+                )
 
 
 if __name__ == "__main__":
